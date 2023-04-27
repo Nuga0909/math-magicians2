c89a26bce1f8e402ab84e699b878a42cfa8aefb2
Adeshina Adenuga <Nuga0909@gmail.com>
2023-04-27 19:42:58 +0100

    remove quote.css

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './quote.css';
 
-const Quote = () => {
+export default function Quote() {
   const [quote, setQuote] = useState('');
   const [hasErr, setHasErr] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -28,13 +28,13 @@ const Quote = () => {
   if (isLoading) {
     return <p className="loading">Loading...</p>;
   }
+
   if (hasErr) return <div>Something went wrong!</div>;
+
   return (
-    <div className="quote">
+    <div className="aquote">
       <blockquote>{`"${quote.quote}"`}</blockquote>
       <p className="author">{quote.author}</p>
     </div>
   );
-};
-
-export default Quote;
+}
