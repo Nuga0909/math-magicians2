6de8bf07a90686fce9e7ed433e10713889e082e8
Adeshina Adenuga <Nuga0909@gmail.com>
2023-05-04 05:36:02 +0100

    refactor and update app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './Calculator.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Nav';
+import HomePage from './components/Home';
 import Quote from './components/Quote';
 
-const App = () => (
-  <div>
-    <Calculator />
-    <Quote />
-  </div>
-);
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  );
+}
 
 export default App;
